1c3c35de2be3583f378c4dd2ba88d2ca5ce49f76
Asad06124 <asadbalqani@gmail.com>
2024-10-28 20:21:45 +0500

    "Added metadata export and imported type from 'next'; updated imports and added metadata object"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // pages/index.tsx or pages/home/page.tsx (depending on your folder structure)
 import Head from 'next/head';
+import type { Metadata } from "next";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
@@ -7,7 +8,11 @@ import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
+export const metadata: Metadata = {
+  title: 'Green Vision - Sustainable Water Solutions in Pakistan',
+  description: 'Explore sustainable water management and irrigation solutions with Green Vision. Join the water revolution in Pakistan with eco-friendly practices.',
+  // metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function Home() {
   return (
     <>
